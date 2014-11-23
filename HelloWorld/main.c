3e3e96f74725d7ef6a5557b8e10fe319c1e80bf4
Vladislav <vladislav@MacBook-Pro-Vladislav.local>
2014-11-23 23:16:45 +0600

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Vladislav on 23.11.14.
+//  Copyright (c) 2014 Vladislav. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
